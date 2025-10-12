f1674fd6eda965c6b1924a61e839e22efd0c321c
Olya <olka.oleinik@gmail.com>
2025-10-12 16:37:43 +0300

    Added: task 1, task 2, task 3, task 4-1, task 4-2, task 4-3

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+let name1 = 'Ivan';
+let name2 = 'Olena';
+
+let greeting = 'Hello ' + name1 + ' and' + ' ' + name2;
+console.log(greeting);
+
+console.log(`Hello ${name1} and ${name2}`); 
---
+++ jsBasics/task4-3.js
@@ -0,0 +1,5 @@
+let radius = 10;
+let height = 10;
+
+let cylinderVolume = Math.PI * (Math.pow (radius, 2)) * height;
+console.log(cylinderVolume.toFixed(2));
