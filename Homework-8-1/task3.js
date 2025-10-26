32a83ceb9282fedca8e5e7ae685e17d41cb58b6f
Olya <olka.oleinik@gmail.com>
2025-10-26 20:26:32 +0200

    Homework 8

---
+++ Homework-8-1/task3.js
@@ -0,0 +1,3 @@
+const numbers = [10, 20, 30, 40, 50];
+const sum = numbers.reduce((previous, current) => previous + current);
+console.log(sum);
---
+++ Homework-8-1/task5.js
@@ -0,0 +1,4 @@
+const firstArray = [1, 3, 5, 7];
+const secondArray = [2, 4, 6, 8];
+const union = firstArray.concat(secondArray);
+console.log(union); 
